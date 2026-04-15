3b6bc2848adfba6d71c419a22e6774f488d44914
9527cherish <488655173@qq.com>
2026-04-15 22:10:30 +0800

    Add online synchronization function

--- include/client/GameInterface/gameScene.hpp
+++ include/client/GameInterface/gameScene.hpp
@@ -5,13 +5,20 @@
 class GameScene : public GameInterface
 {
 public:
+    GameScene();
+    ~GameScene() override;
+
     void renderScene() override;
     void renderCurrentPerson();
-
+    void renderBackground(int cameraX, int cameraY);
+    void renderRemotePersons(int cameraX, int cameraY, Uint32 deltaTime);
 
     void handleEvent(const SDL_Event &e) override;
     void initScene() override;
 private:
-    SDL_Renderer* m_pRenderer;
+    SDL_Renderer* m_pRenderer = nullptr;
+    SDL_Texture* m_pBackgroundTexture = nullptr;
+    int m_iBackgroundWidth = 0;
+    int m_iBackgroundHeight = 0;
     Uint32 m_lastFrameTime = 0;
 };
--- src/client/GameInterface/gameScene.cpp
+++ src/client/GameInterface/gameScene.cpp
@@ -1,14 +1,27 @@
 #include "gameScene.hpp"
+#include <algorithm>
+#include <SDL2/SDL_image.h>
+#include <spdlog/spdlog.h>
 #include "persona.hpp"
 #include "interfaceManager.hpp"
 #include "dataManager.hpp"
 #include "netClient.hpp"
 
+GameScene::GameScene()
+{
+}
+
+GameScene::~GameScene()
+{
+    if (m_pBackgroundTexture != nullptr)
+    {
+        SDL_DestroyTexture(m_pBackgroundTexture);
+        m_pBackgroundTexture = nullptr;
+    }
+}
+
 void GameScene::renderScene()
 {
-    SDL_SetRenderDrawColor(m_pRenderer, 0, 0, 0, 255);
-    SDL_RenderClear(m_pRenderer);
-
     Uint32 now = SDL_GetTicks();
     if (m_lastFrameTime == 0) {
         m_lastFrameTime = now;
@@ -17,21 +30,16 @@ void GameScene::renderScene()
     m_lastFrameTime = now;
 
     std::shared_ptr<Persona> persona = DataManager::getInstance().currentPersona();
+    int cameraX = SCREEN_WIDTH / 2;
+    int cameraY = SCREEN_HEIGHT / 2;
     if (persona != nullptr) {
         persona->tick(deltaTime);
+        cameraX = persona->x();
+        cameraY = persona->y();
     }
 
-    std::vector<std::shared_ptr<Persona>> remotePersonas;
-    DataManager::getInstance().getRemotePersonas(remotePersonas);
-    for (const std::shared_ptr<Persona>& remotePersona : remotePersonas)
-    {
-        if (remotePersona == nullptr) {
-            continue;
-        }
-        remotePersona->tick(deltaTime);
-        remotePersona->rendererCurPersonaScaled(m_pRenderer, 1.5f);
-    }
-
+    renderBackground(cameraX, cameraY);
+    renderRemotePersons(cameraX, cameraY, deltaTime);
     renderCurrentPerson();
 }
 
@@ -44,7 +52,51 @@ void GameScene::renderCurrentPerson()
         return;
     }
 
-    persona->rendererCurPersonaScaled(m_pRenderer, 1.5f);
+    persona->rendererCurPersonaScaled(m_pRenderer, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 1.5f);
+}
+
+void GameScene::renderBackground(int cameraX, int cameraY)
+{
+    if (m_pRenderer == nullptr)
+    {
+        return;
+    }
+
+    if (m_pBackgroundTexture == nullptr)
+    {
+        SDL_SetRenderDrawColor(m_pRenderer, 18, 24, 26, 255);
+        SDL_RenderClear(m_pRenderer);
+        return;
+    }
+
+    const int maxX = std::max(0, m_iBackgroundWidth - SCREEN_WIDTH);
+    const int maxY = std::max(0, m_iBackgroundHeight - SCREEN_HEIGHT);
+    const int sourceX = std::clamp(cameraX - SCREEN_WIDTH / 2, 0, maxX);
+    const int sourceY = std::clamp(cameraY - SCREEN_HEIGHT / 2, 0, maxY);
+
+    SDL_Rect srcRect{sourceX, sourceY,
+                     std::min(SCREEN_WIDTH, m_iBackgroundWidth),
+                     std::min(SCREEN_HEIGHT, m_iBackgroundHeight)};
+    SDL_Rect dstRect{0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
+
+    SDL_RenderCopy(m_pRenderer, m_pBackgroundTexture, &srcRect, &dstRect);
+}
+
+void GameScene::renderRemotePersons(int cameraX, int cameraY, Uint32 deltaTime)
+{
+    std::vector<std::shared_ptr<Persona>> remotePersonas;
+    DataManager::getInstance().getRemotePersonas(remotePersonas);
+    for (const std::shared_ptr<Persona>& remotePersona : remotePersonas)
+    {
+        if (remotePersona == nullptr) {
+            continue;
+        }
+
+        remotePersona->tick(deltaTime);
+        const int screenX = SCREEN_WIDTH / 2 + (remotePersona->x() - cameraX);
+        const int screenY = SCREEN_HEIGHT / 2 + (remotePersona->y() - cameraY);
+        remotePersona->rendererCurPersonaScaled(m_pRenderer, screenX, screenY, 1.5f);
+    }
 }
 
 void GameScene::handleEvent(const SDL_Event &e)
@@ -64,6 +116,20 @@ void GameScene::initScene()
 {
     if(nullptr == m_pRenderer)
         m_pRenderer = InterfaceManager::getInstance().renderer();
+
+    if (m_pBackgroundTexture == nullptr && m_pRenderer != nullptr)
+    {
+        m_pBackgroundTexture = IMG_LoadTexture(m_pRenderer, "../images/tmwa/graphics/images/login_wallpaper.png");
+        if (m_pBackgroundTexture == nullptr)
+        {
+            spdlog::error("加载游戏背景失败: {}", IMG_GetError());
+        }
+        else
+        {
+            SDL_QueryTexture(m_pBackgroundTexture, nullptr, nullptr, &m_iBackgroundWidth, &m_iBackgroundHeight);
+        }
+    }
+
     if (m_lastFrameTime == 0) {
         m_lastFrameTime = SDL_GetTicks();
     }
